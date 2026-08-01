096e87af96a6e3d5bca415db08a2e2308258a92a
zooma123 <93126188+zooma123@users.noreply.github.com>
2026-08-01 16:29:05 +0300

    Create app/Modules/Auth/Models/User.php

---
+++ app/Modules/Auth/Models/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Modules\Auth\Models;
+
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use App\Modules\Projects\Models\Project;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    protected static function newFactory()
+    {
+        return UserFactory::new();
+    }
+}
