13fa3c74fceef106f688ac30825d7480e4b1e2ea
zooma123 <93126188+zooma123@users.noreply.github.com>
2026-08-01 16:07:36 +0300

    Update routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,6 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('tasks:check-overdue')->dailyAt('09:00');
