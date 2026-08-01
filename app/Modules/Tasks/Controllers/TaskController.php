8248aaf32c7c184d13e0b1c706769000f5cfd9a4
zooma123 <93126188+zooma123@users.noreply.github.com>
2026-08-01 17:22:34 +0300

    Create app/Modules/Tasks/Controllers/TaskController.php

---
+++ app/Modules/Tasks/Controllers/TaskController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Modules\Tasks\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Modules\Tasks\Requests\StoreTaskRequest;
+use App\Modules\Tasks\Requests\UpdateTaskRequest;
+use App\Modules\Tasks\Resources\TaskCollection;
+use App\Modules\Tasks\Resources\TaskResource;
+use App\Modules\Tasks\Services\TaskService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    protected TaskService $taskService;
+
+    public function __construct(TaskService $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
+    public function index(Request $request): JsonResponse|TaskCollection
+    {
+        $filters = $request->only(['status', 'priority', 'title', 'per_page', 'sort_by', 'sort_order']);
+        $service = $this->taskService->listTasks($filters);
+
+        if ($service->hasError()) {
+            return response()->json(['error' => $service->getError()], $service->getErrorCode());
+        }
+
+        return new TaskCollection($service->getData());
+    }
+
+    public function store(StoreTaskRequest $request): JsonResponse|TaskResource
+    {
+        $service = $this->taskService->createTask($request->validated());
+
+        if ($service->hasError()) {
+            return response()->json(['error' => $service->getError()], $service->getErrorCode());
+        }
+
+        return (new TaskResource($service->getData()))->response()->setStatusCode(201);
+    }
+
+    public function update(UpdateTaskRequest $request, int $id): JsonResponse|TaskResource
+    {
+        $service = $this->taskService->updateTask($id, $request->validated());
+
+        if ($service->hasError()) {
+            return response()->json(['error' => $service->getError()], $service->getErrorCode());
+        }
+
+        return new TaskResource($service->getData());
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $service = $this->taskService->deleteTask($id);
+
+        if ($service->hasError()) {
+            return response()->json(['error' => $service->getError()], $service->getErrorCode());
+        }
+
+        return response()->json($service->getData(), 200);
+    }
+}
