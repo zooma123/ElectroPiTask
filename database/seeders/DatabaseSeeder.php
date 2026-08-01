810e902300b5313904e253e193843f2d616c37f9
zooma123 <93126188+zooma123@users.noreply.github.com>
2026-08-01 15:53:56 +0300

    Update database/seeders/DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Modules\Auth\Models\User;
+use App\Modules\Projects\Models\Project;
+use App\Modules\Tasks\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(5)->create()->each(function ($user) {
+            Project::factory(3)->create(['user_id' => $user->id])->each(function ($project) {
+                Task::factory(5)->create(['project_id' => $project->id]);
+            });
+        });
     }
 }
